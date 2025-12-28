06df2588b2e4dda6638e57f1c0b397430ae2563a
Wambia Kennedy <kennyleyy0@gmail.com>
2025-12-28 12:56:24 +0300

    Started Backend

---
+++ backend/routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/admins', [AdminController::class, 'index']);
+Route::post('/admins', [AdminController::class, 'store']);
+Route::put('/admins/{id}', [AdminController::class, 'update']);
+Route::delete('/admins/{id}', [AdminController::class, 'destroy']);
+Route::patch('/admins/{id}/toggle-status', [AdminController::class, 'toggleStatus']);
